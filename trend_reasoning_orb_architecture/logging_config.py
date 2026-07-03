2642c3f160273eed17083689a63ced694a3eb9ac
Sanjay G <gokulesh2615@gmail.com>
2026-07-03 14:29:23 +0530

    feat(logging_config): implement logging_config module

---
+++ trend_reasoning_orb_architecture/logging_config.py
@@ -0,0 +1,24 @@
+import logging
+
+# Create a logger
+logger = logging.getLogger(__name__)
+
+# Set the logging level
+logger.setLevel(logging.INFO)
+
+# Create a file handler
+file_handler = logging.FileHandler('trend_reasoning_orb_architecture.log')
+file_handler.setLevel(logging.INFO)
+
+# Create a console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Create a formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
